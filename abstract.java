76b63f1d27ffb50e30f47565d46812140347df08
Aliza Khan <124785215+AlizaKhan3@users.noreply.github.com>
2024-05-15 22:31:32 +0500

    Create abstract.java
    abstract details

---
+++ abstract.java
@@ -0,0 +1,8 @@
+  //Aesi class hoti hai jis k ander koi na koi eik abstract method ho, matlab 
+    //jese mne ek method yaani function banaya public void ICOOK(){  } lekin ab mjhe pta hai i cannot cook, 
+    //inshort hua kia ye function mere ander kaam nhi karrha or iski koi method body nhi toh aesa function
+    //hamare ps abstract hota hai.   
+    //abstract function k saare brackets hata kr ek collon (;) lagadiya jata hai
+    //aese method k saath mein abstract likhna mandatory taa k pta chale k ye abstract hai
+    //abstract class cannot used to create objects
+    //----> abstract (small 'a' se ata hai)
